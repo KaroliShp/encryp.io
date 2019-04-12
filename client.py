ed334bf37de67eb8e17db4dd7b68a472a224082f
Karolishp <karolis.spukas@gmail.com>
2019-04-13 02:43:26 +0300

    Half of milestone 1

--- client.py
+++ client.py
@@ -1,19 +1,37 @@
-import socket
+from socket import AF_INET, socket, SOCK_STREAM
+import threading
+
+HOST = '192.168.1.188'
+PORT = 5000
+
+BUFSIZ = 1024
+
+
+def send_message(client_socket):
+    while True:
+        message = input('You: ')
+        client_socket.send(message.encode())
 
 def main():
-    host = '127.0.0.1'
-    port = 5001
+    print(f'Starting the client...')
 
-    s = socket.socket()
-    s.connect((host, port))
+    client_socket = socket(AF_INET, SOCK_STREAM)
+    print(f'Client socket created successfully')
 
-    message = input("Input: ")
-    while message != 'q':
-        s.send(message.encode())
-        data = s.recv(1024)
-        print(f'Received from the server: {str(data)}')
-        message = input("Input: ")
-    s.close()
+    client_socket.connect((HOST, PORT))
+    print(f'Connected to: {HOST}:{str(PORT)}')
+
+    send_thread = threading.Thread(target=send_message, args=(client_socket,))
+    send_thread.daemon = True
+    send_thread.start()
+
+    while True:
+        data = client_socket.recv(BUFSIZ).decode()
+
+        if not data:
+            break
+
+        print(f'{data}\n')
 
 if __name__ == '__main__':
     main()
--- server.py
+++ server.py
@@ -1,30 +1,50 @@
 import socket
+import threading
+import sys
 
-def main():
-    host = '127.0.0.1'
-    port = 5001
+ADDR = '0.0.0.0'
+PORT = 5000
 
-    s = socket.socket()
-    s.bind((host, port))
+BUFF_SIZE = 1024
 
-    s.listen(1)
+connections = []
 
-    c, addr = s.accept()
+def broadcast(message, c):
+    print(f'(Broadcasting) {message}')
+    for connection in connections:
+        if c != connection:
+            connection.send(message.encode())
 
-    print(f'Connection from {str(addr)}')
-
+def client_handler(c, ca):
     while True:
-        data = c.recv(1024)
+        data = c.recv(BUFF_SIZE).decode()
 
+        broadcast(f'{ca}: {data}', c)
+        
         if not data:
+            print(f'Connection with {ca} has been lost')
             break
 
-        print(f'From connected user {str(data)}')
-        data = str(data).upper()
-        print(f'Sending back: {str(data)}')
-        c.send(data.encode())
+def main():
+    print(f'Starting the server...')
 
-    c.close()
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)    
+    print(f'Socket created successfully...')
 
+    s.bind((ADDR, PORT))
+    print(f'Socket binded to {ADDR}:{str(PORT)} successfully...')
+
+    s.listen(2)
+
+    while True:
+        client, client_address = s.accept()
+        print(f'Client {client_address} has connected to the server')
+
+        client_thread = threading.Thread(target=client_handler, args=(client, client_address))
+        client_thread.daemon = True
+        client_thread.start()
+
+        connections.append(client)
+    
 if __name__ == '__main__':
     main()
